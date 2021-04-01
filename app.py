f68c905ff94423be707ab8cbbb39d5d9c9327ecd
Isabella <isy_smales@hotmail.co.uk>
2021-04-01 16:56:13 +0100

    Generate multiple exercises

--- app.py
+++ app.py
@@ -13,6 +13,12 @@ import random
 @app.route('/one_exercise')
 def one_exercise():
     no_of_rows = Back.query.count()
-    random_number = random.randrange(1, no_of_rows + 1)
-    random_exercise = Back.query.get(random_number)
-    return f'{random_exercise.exercise_name}' 
+    random_sample = random.sample(range(1, no_of_rows + 1), 5)
+    list_of_exercises = []
+    for i in random_sample:
+        random_exercise = Back.query.get(i)
+        list_of_exercises.append(random_exercise.exercise_name)
+    return f'{list_of_exercises}' 
+
+
+    
