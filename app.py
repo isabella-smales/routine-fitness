1289b771b86da59eab0ceca581bb1a27fb72dcbd
Isabella <isy_smales@hotmail.co.uk>
2021-04-06 11:53:07 +0100

    Time adding

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -10,9 +10,18 @@ from models import Back
 
 import random
 
-@app.route('/')
+
+@app.route('/', methods=['GET'])
 def home():
-    return render_template('home.html', title='Home Page')
+    times = ['Short (30 mins)', 'Medium (1 hour)', 'Long (1 hour 30 mins)']
+    muscle_groups = ['Back', 'Chest', 'Arms', 'Legs']
+    return render_template('home.html', title='Home Page', times=times, muscle_groups=muscle_groups)
+
+
+@app.route('/result', methods=['GET', 'POST'])
+def result():
+    select = request.form.get(times)
+    return(str(select))
 
 @app.route('/one_workout')
 def one_workout():
@@ -24,3 +33,4 @@ def one_workout():
         list_of_exercises.append(random_exercise.exercise_name)
         one_workout = str(list_of_exercises)[1:-1]
     return render_template('one_workout.html', title='One Workout') + f'{one_workout}' 
+
