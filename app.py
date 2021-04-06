9da47b434dfa57e373b3742878d4e668eac9b5c3
Jaishal Mehta <jaishalmehta@Jaishals-MacBook-Air.local>
2021-04-06 16:42:53 +0100

    Passed time as an arguement

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, request, url_for
 from flask_sqlalchemy import SQLAlchemy
+from forms import InputForm
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./data/exercises.sqlite'
@@ -18,12 +19,29 @@ def home():
     if not form.validate_on_submit():
         return render_template('home.html', form=form)
     time = form.time.data
-    print(form.time.choices[0][0]) #short 
-    print(form.time.choices[1][0]) #medium
-    print(form.time.choices[2][0]) #long
-    if time == form.time.choices[1][0]:
-        return redirect('/one_workout')
-    # times = ['Short (30 mins)', 'Medium (1 hour)', 'Long (1 hour 30 mins)']
+    if request.method == "POST":
+        return redirect(url_for("one_workout", time=time))
+    else: 
+        return render_template("home.html")
+    
+@app.route("/<time>")
+def one_workout(time):
+    if time == 'Short (30 mins)':
+        no_of_rows = Back.query.count()
+        random_sample = random.sample(range(1, no_of_rows + 1), 5)
+        list_of_exercises = []
+        for i in random_sample:
+            random_exercise = Back.query.get(i)
+            list_of_exercises.append(random_exercise.exercise_name)
+            one_workout = str(list_of_exercises)[1:-1]
+        return render_template('one_workout.html', title='One Workout') + f'{one_workout}' + f"<h1>{time}</h1>"
+
+   # print(form.time.choices[0][0]) #short 
+  #  print(form.time.choices[1][0]) #medium
+  #  print(form.time.choices[2][0]) #long
+   # if time == form.time.choices[1][0]:
+   #     return redirect('/one_workout')
+  #  # times = ['Short (30 mins)', 'Medium (1 hour)', 'Long (1 hour 30 mins)']
     # muscle_groups = ['Back', 'Chest', 'Arms', 'Legs']
     #return render_template('home.html', title='Home Page')#, times=times, muscle_groups=muscle_groups)
 
@@ -34,16 +52,3 @@ def home():
 
 #@app.route('/form', methods = ['GET', 'POST'])
 #def form():
-    
-
-
-@app.route('/one_workout')
-def one_workout():
-    no_of_rows = Back.query.count()
-    random_sample = random.sample(range(1, no_of_rows + 1), 5)
-    list_of_exercises = []
-    for i in random_sample:
-        random_exercise = Back.query.get(i)
-        list_of_exercises.append(random_exercise.exercise_name)
-        one_workout = str(list_of_exercises)[1:-1]
-    return render_template('one_workout.html', title='One Workout') + f'{one_workout}' 
