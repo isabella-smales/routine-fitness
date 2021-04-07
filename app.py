ea00f812f73d4fba8b701012832782d77f90d4ee
Isabella <isy_smales@hotmail.co.uk>
2021-04-07 16:58:57 +0100

    Choose from Tricep/Bicep/Shoulder exercises

--- app.py
+++ app.py
@@ -18,7 +18,6 @@ def home():
     if not form.validate_on_submit():
         return render_template('home.html', form=form)
     time = form.time.data
-
     muscle_group = form.muscle_group.data
     muscle1 = muscle_group[0]
     if len(muscle_group) != 1:
@@ -33,7 +32,7 @@ def home():
     
 @app.route("/<time>/<muscle1>/<muscle2>")
 def one_workout(time, muscle1, muscle2):
-    if time == 'Short (30 mins)':
+    if time == 'Short (30 mins)':   #time selection
         no_of_exercises = 4
     elif time == 'Medium (1 hour)':
         no_of_exercises = 6
@@ -42,21 +41,33 @@ def one_workout(time, muscle1, muscle2):
     
     list_of_exercises = []
 
-    if muscle2 == muscle1:
+    if muscle2 == muscle1:  # muscle group selection
         list = [muscle1]
     else:
         list = [muscle1, muscle2]
+
     for muscle in list:
-        if len(list) == 1:
+        if muscle == 'Biceps' or muscle == 'Triceps' or muscle == 'Shoulders':  # Calling rows for biceps
+            arm_subgroup = Arms.query.filter(Arms.arm_subgroup == muscle).all()
+            no_of_rows = len(arm_subgroup)
+        else:   # else rows for all else
             no_of_rows = eval(muscle).query.count()
+
+        if len(list) == 1:  # link to muscle group/time selectors
             random_sample = random.sample(range(1, no_of_rows + 1), min(no_of_exercises, no_of_rows))
         else:
-            no_of_rows = eval(muscle).query.count()
             random_sample = random.sample(range(1, no_of_rows + 1), min(int(no_of_exercises/2), no_of_rows))
-        for i in random_sample:
-            random_exercise = eval(muscle).query.get(i)
+            
+        for i in random_sample: #
+            if muscle =='Biceps' or muscle == 'Triceps' or muscle == 'Shoulders':
+                random_exercise = arm_subgroup[i-1]
+            else:
+                random_exercise = eval(muscle).query.get(i)    #random_exercise = biceps[i-1].exercise_name
             list_of_exercises.append(random_exercise.exercise_name)
             one_workout = str(list_of_exercises)[1:-1]
-    
+
     return render_template('one_workout.html', title='One Workout') + f'{one_workout}' + f"<h1>{time}</h1>"
+
+
+
 
--- forms.py
+++ forms.py
@@ -4,6 +4,5 @@ from wtforms.validators import DataRequired
 
 class InputForm(FlaskForm):
     time = SelectField('Workout length', choices = [ 'Short (30 mins)', 'Medium (1 hour)', 'Long (1 hour 30 mins)'], validators=[DataRequired()])    
-    muscle_group = SelectMultipleField('Muscle Group', choices = [('Back', 'BK'), ('Chest', 'CH'), ('Arms', 'AR'), ('Legs', 'LG')], validators=[DataRequired()])
+    muscle_group = SelectMultipleField('Muscle Group', choices = [('Back', 'BK'), ('Chest', 'CH'), ('Arms', 'AR'), ('Biceps', 'BI'), ('Triceps','TR'), ('Shoulders','SH'), ('Legs', 'LG')], validators=[DataRequired()])
     submit = SubmitField('Submit')
-j
