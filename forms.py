21747f93a237d2f60310c0bbbba8e46b35eb1851
Jaishal Mehta <jaishalmehta@Jaishals-MacBook-Air.local>
2021-04-08 19:27:05 +0100

    Fixed minor changes

--- forms.py
+++ forms.py
@@ -1,7 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import SelectField, SubmitField, SelectMultipleField, StringField, PasswordField
 from wtforms.validators import DataRequired, EqualTo, ValidationError
- #from models import User
 
 class HomeForm(FlaskForm):
     type_of_routine = SelectField('Choose your Workout', choices = ['Daily Routine', 'Weekly Routine'], validators=[DataRequired()])
@@ -22,11 +21,6 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
-
-    #def validate_username(self, username):
-      #  user = User.query.filter_by(username=username.data).first()
-       # if user:
-        #    raise ValidationError('That username is taken. Please choose a different one.')
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
